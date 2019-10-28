c9618d4c6ac691f4e5c9f310ac9f38793d511f80
unknown <shalmayev1993@gmail.com>
2019-10-28 19:13:02 +0200

    chunk solution

--- chunk/index.js
+++ chunk/index.js
@@ -8,6 +8,33 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    /** SOLUTION 1 **/
+    // let newArr = [];
+    // while(array.length){
+    //   newArr.push(array.splice(0, size));
+    // }
+    // return newArr;
+
+    /** SOLUTION 2 **/
+    // Create empty array to hold chunks
+    const chunked = [];
+    // for each element in the unchunked array
+    for(let element of array){
+        // retrieve the last element in chunked
+        const last = chunked[chunked.length -1];
+        // if last element does not exists or if its length is equal to chunk size
+        if(!last || last.length === size){
+            // push new chunk into chunked with current element
+            chunked.push([element]);
+        }else{
+            // add the curreent element into the chunked
+            last.push(element);
+        }
+    }
+    
+    return chunked;
+
+}
 
 module.exports = chunk;
