aa6b6952fbb1041764050ded79fd766909c85793
classuser2 <classuser2@HQ.Taasuka.gov.il>
2019-10-28 15:02:18 +0200

    solutions for reverseInt

--- reverseint/index.js
+++ reverseint/index.js
@@ -8,6 +8,21 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    /** SOLUTION 1 **/
+    // Math.sign return if number is positive 1 else -1
+    // tilde returns -1 to 0, 
+    // if the number given is negative then do this..
+    // if(!~Math.sign(n)){
+    //     return -parseInt(n.toString().split``.reverse``.join``);
+    // }else{
+    //     return parseInt(n.toString().split``.reverse``.join``);
+    // }
+
+    /** SOLUTION 2 **/
+    let reversed = n.toString().split``.reverse``.join``;
+    return parseInt(reversed) * Math.sign(n);
+
+}
 
 module.exports = reverseInt;
