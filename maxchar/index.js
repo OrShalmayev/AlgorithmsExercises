75529c0a74f5d92fb6956ff3e266fe2a9398e0fd
classuser2 <classuser2@HQ.Taasuka.gov.il>
2019-10-28 15:45:15 +0200

    solution for maxchar

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    /*********************** SOLUTION 1 ***********************/
+    let obj = {};
+    let char = '';
+    let numShown = 0;
+    for (let i = 0; i < str.length; i++){
+        if(!str[i] in obj){
+            obj[str[i]] = 1
+        }else if(str[i] in obj){
+            obj[str[i]]++;
+        }
+    }
+
+    for(let k in obj){
+        if(obj[k] > numShown){
+            numShown = obj[k];
+            char = k; 
+        }
+    }
+    return char;
+}
 
 module.exports = maxChar;
