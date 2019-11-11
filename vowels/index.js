ecb02bff657de94468c5f28c62bcfdcc0de9278d
unknown <shalmayev1993@gmail.com>
2019-11-11 18:50:05 +0200

    solution for vowels

--- vowels/index.js
+++ vowels/index.js
@@ -7,6 +7,21 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    let counter = 0;
+    for(let c of str){
+        if('aeiou'.includes(c.toLowerCase())) counter++;
+    }
+    return counter;
+}
 
 module.exports = vowels;
+
+/** SOLUTION 1 **/
+function vowels(str) {
+    let counter = 0;
+    for(let c of str){
+        if('aeiou'.includes(c.toLowerCase())) counter++;
+    }
+    return counter;
+}
